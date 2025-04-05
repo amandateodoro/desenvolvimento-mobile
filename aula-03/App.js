b24d43c68bfbd3545c951f181604324550274a97
Amanda Teodoro <85532804+amandateodoro@users.noreply.github.com>
2025-04-04 22:16:47 -0400

    aula-03

--- aula-03/App.js
+++ aula-03/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AtvComponentesBasicos from './Components/AtvComponentesBasicos';
 
 export default function App() {
   return (
